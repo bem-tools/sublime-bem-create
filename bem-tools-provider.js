7a11fef0b130233760e282224288f2bfa1f661c5
Vladislav Khvostov <khvostov@yamoney.ru>
2016-07-04 11:12:20 +0300

    added demo

--- bem-tools-provider.js
+++ bem-tools-provider.js
@@ -1,8 +1,6 @@
 const bemToolsCreate = require('bem-tools-create')
 const getStdin = require('get-stdin');
-const fs = require('fs');
 
-// Example enters data - [[{"block": "vitaly"}], [{"path": "C:/sublime/Data/Packages/BemTools"}], [{"techs": "['css', 'js']"}]]
 getStdin().then(options => {
 	const entitiesData = JSON.parse(options)
 	const {block, paths, techs} = entitiesData;
