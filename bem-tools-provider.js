b42e5896c606de2c670c115c93beb6aba3d96a60
Vladislav Khvostov <khvostov@yamoney.ru>
2016-07-04 03:03:33 +0300

    remove extra comments

--- bem-tools-provider.js
+++ bem-tools-provider.js
@@ -2,22 +2,9 @@ const bemToolsCreate = require('bem-tools-create')
 const getStdin = require('get-stdin');
 const fs = require('fs');
 
-// bemToolsCreate([[{"block": "vitaly"}], [{"path": "C:/sublime/Data/Packages/BemTools"}], [{"techs": "['css', 'js']"}]]);
+// Example enters data - [[{"block": "vitaly"}], [{"path": "C:/sublime/Data/Packages/BemTools"}], [{"techs": "['css', 'js']"}]]
 getStdin().then(options => {
-	const obj = JSON.parse(options)
-	const {block, paths, techs} = obj;
+	const entitiesData = JSON.parse(options)
+	const {block, paths, techs} = entitiesData;
 	bemToolsCreate(block, paths, techs)
-
 });
-
-
-// 	const {blocks, paths, techs} = options;
-
-
-//[
-//     { block: 'vitaly-harisov' },
-//     { block: 'vitaly-harisov', modName: 'm1', modVal: true },
-//     { block: 'vitaly', elem: 'e2' },
-//     { block: 'vitaly', elem: 'e2', modName: 'elemMod', modVal: true },
-//     { block: 'harisov', modName: 'm1', modVal: 'v1' }
-// ], ['tmp', 'tmp2', 'path/to/level'], ['css', 'js', 'bemhtml.js']
