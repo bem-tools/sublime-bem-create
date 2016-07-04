be437c0d5a8c923f012ab5dbb0afcac259d23050
Vladislav Khvostov <khvostov@yamoney.ru>
2016-07-04 09:25:44 +0300

    added user's input

--- BemTools.py
+++ BemTools.py
@@ -12,17 +12,12 @@ BIN_PATH = join(sublime.packages_path(), dirname(realpath(__file__)), 'bem-tools
 
 class BemToolsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		settings = {
-			'block': [
-				{ 'block': "vitaly", "elem": "e2" },
-				{ 'block': 'vitaly', 'elem': 'e2', 'modName': 'elemMod', 'modVal': 'true' },
-				{ 'block': 'vlad'}
-			],
-			'paths': ['C:/sublime/Data/Packages/BemTools'],
-			'techs': ["css", "js"]
-		}
-		convertToJson = json.dumps(settings)
-		try:
-			node_bridge(convertToJson, BIN_PATH)
-		except Exception as e:
-			sublime.error_message('bem-create\n%s' % e)
+		# Execute code with node.js
+		def node_exec(data):
+			try:
+				node_bridge(data, BIN_PATH)
+			except Exception as e:
+				sublime.error_message('bem-create\n%s' % e)
+		# Open input and get user data, then use callback.
+		self.view.window().show_input_panel('Block name, path, techs', "{\"block\":{\"block\":\"khvostov\"},\"paths\":[\"C:/path/to/ur/proj\"],\"techs\":[\"css\",\"js\", \"bemjson\"]}", node_exec, None, None)
+
--- bem-tools-provider.js
+++ bem-tools-provider.js
@@ -1,6 +1,7 @@
 const bemToolsCreate = require('bem-tools-create')
 const getStdin = require('get-stdin');
 const fs = require('fs');
+console.log('Node was started\n')
 
 // Example enters data - [[{"block": "vitaly"}], [{"path": "C:/sublime/Data/Packages/BemTools"}], [{"techs": "['css', 'js']"}]]
 getStdin().then(options => {
