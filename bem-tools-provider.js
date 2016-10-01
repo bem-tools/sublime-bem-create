ee999745ea27df02a73a7b24da749e0354963164
Roman Ponomarev <maksugr@gmail.com>
2016-10-01 14:06:47 +0300

    Fixes #1

--- bem-tools-provider.js
+++ bem-tools-provider.js
@@ -1,8 +1,9 @@
-const bemToolsCreate = require('bem-tools-create')
+'use strict';
+
+const bemToolsCreate = require('bem-tools-create');
 const getStdin = require('get-stdin');
 
 getStdin().then(options => {
-	const entitiesData = JSON.parse(options)
-	const {block, paths, techs} = entitiesData;
-	bemToolsCreate(block, paths, techs)
+	const entitiesData = JSON.parse(options);
+	bemToolsCreate(entitiesData.block, entitiesData.paths, entitiesData.techs);
 });
